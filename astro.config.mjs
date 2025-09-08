633c7c71ac630132e84f3588c7aad5839c157557
Jevaughn Newman <jevaughn.newman@gmail.com>
2025-09-08 18:04:48 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://jevaughnnewman.github.io',
-  base: '/portfolio/',
+  base: '/portfolio',
+  output: 'static',
+  integrations: [tailwind()],
 });
+
 
